7ab3b0bd7b592cbf10ca1dbd7b9c338920eeb90c
xinmeng94 <xyaimao@gmail.com>
2022-07-13 16:20:28 +0200

    cambio 3

--- api.py
+++ api.py
@@ -1,2 +1,9 @@
 from flask import Flask, render_template
 from src import covid_dash, hospitals_tb
+
+app = Flask(__name__)
+@app.route("/")
+def landing_page():
+return render_template('index.html')
+if __name__ == '__main__':
+app.run(host='0.0.0.0',port=1991, debug=True)
