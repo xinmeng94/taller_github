160e8f57afadf6f889c209008f5cdc9f2fed0fe7
saradevera <saradevv@gmail.com>
2022-07-13 16:30:42 +0200

    cambio5

--- covid_dash.py
+++ covid_dash.py
@@ -44,3 +44,130 @@ top10_recovered = df_top10["Recovered"].tolist()
 df_top10 = df_total.nlargest(10, "Deaths")
 top10_countries_3 = df_top10["Country_Region"].tolist()
 top10_deaths = df_top10["Deaths"].tolist()
+fig = make_LO_QUE_TE_DE_LA_GANA(
+rows = 4, cols = 6,
+specs=[
+[{"type": "scattergeo", "rowspan": 4, "colspan": 3},
+None, None, {"type": "indicator"}, {"type": "indicator"},
+{"type": "indicator"} ],
+[ None, None, None, {"type": "bar",
+"colspan":3}, None, None],
+[ None, None, None, {"type": "bar",
+"colspan":3}, None, None],
+[ None, None, None, {"type": "bar",
+"colspan":3}, None, None],
+]
+)
+message = df_final["Country_Region"] + " " +
+df_final["Province_State"] + "<br>"
+message += "Confirmed: " + df_final["Confirmed"].astype(str) +
+"<br>"
+message += "Deaths: " + df_final["Deaths"].astype(str) + "<br>"
+message += "Recovered: " + df_final["Recovered"].astype(str) +
+"<br>"
+message += "Last updated: " + df_final["Last_Update"].astype(str)
+df_final["text"] = message
+fig.add_trace(
+go.Scattergeo(
+locationmode = "country names",
+lon = df_final["Long_"],
+lat = df_final["Lat"],
+hovertext = df_final["text"],
+showlegend=False,
+marker = dict(
+size = 10,
+opacity = 0.8,
+reversescale = True,
+autocolorscale = True,
+symbol = 'square',
+line = dict(
+width=1,
+color='rgba(102, 102, 102)'
+),
+cmin = 0,
+color = df_final['Confirmed'],
+cmax = df_final['Confirmed'].max(),
+colorbar_title="Confirmed Cases<br>Latest Update",
+colorbar_x = -0.05
+)
+),
+row=1, col=1
+)
+fig.add_trace(
+go.Indicator(
+mode="number",
+value=total_confirmed,
+title="Confirmed Cases",
+),
+row=1, col=4
+)
+fig.add_trace(
+go.Indicator(
+mode="number",
+value=total_recovered,
+title="Recovered Cases",
+),
+row=1, col=5
+)
+fig.add_trace(
+go.Indicator(
+mode="number",
+value=total_deaths,
+title="Deaths Cases",
+),
+row=1, col=6
+)
+fig.add_trace(
+go.Bar(
+x=top10_countries_1,
+y=top10_confirmed,
+name= "Confirmed Cases",
+marker=dict(color="Yellow"),
+showlegend=True,
+),
+row=2, col=4
+)
+fig.add_trace(
+go.Bar(
+x=top10_countries_2,
+y=top10_recovered,
+name= "Recovered Cases",
+marker=dict(color="Green"),
+showlegend=True),
+row=3, col=4
+)
+fig.add_trace(
+go.Bar(
+x=top10_countries_3,
+y=top10_deaths,
+name= "Deaths Cases",
+marker=dict(color="crimson"),
+showlegend=True),
+row=4, col=4
+)
+fig.update_layout(
+template="plotly_dark",
+title = "Global COVID-19 Cases (Last Updated: " +
+str(df_final["Last_Update"][0]) + ")",
+showlegend=True,
+legend_orientation="h",
+legend=dict(x=0.65, y=0.8),
+geo = dict(
+projection_type="orthographic",
+showcoastlines=True,
+landcolor="white",
+showland= True,
+showocean = True,
+lakecolor="LightBlue"
+),
+annotations=[
+dict(
+text="Source: https://bit.ly/3aEzxjK",
+showarrow=False,
+xref="paper",
+yref="paper",
+x=0.35,
+y=0)
+]
+)
+fig.write_html('templates/quedivertidoesesteejercicio.html')
