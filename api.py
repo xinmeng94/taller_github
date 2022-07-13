86e5ad6bdb8d3b7d96a4861016a4980306c4f2e3
xinmeng94 <xyaimao@gmail.com>
2022-07-13 16:30:46 +0200

    cambio 5

--- api.py
+++ api.py
@@ -5,5 +5,13 @@ app = Flask(__name__)
 @app.route("/")
 def landing_page():
 return render_template('index.html')
+
+@app.route("/dashboard")
+def dashboard():
+return render_template('ÑAÑAAÑÑAÑAÑAÑAÑÑA.html')
+@app.route("/map")
+def map():
+return render_template('map.html')
+
 if __name__ == '__main__':
 app.run(host='0.0.0.0',port=1991, debug=True)
